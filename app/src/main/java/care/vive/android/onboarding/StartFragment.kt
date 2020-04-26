7c6f488c0e4837a6a222251dcf0160931f5aa956
Carlos Alberto Murillo <carlosalbertomurillo@gmail.com>
2020-04-26 10:14:48 -0400

    temporal navitation to main map

--- app/src/main/java/care/vive/android/onboarding/StartFragment.kt
+++ app/src/main/java/care/vive/android/onboarding/StartFragment.kt
@@ -1,11 +1,13 @@
 package care.vive.android.onboarding
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import care.vive.android.main.MainActivity
 import com.vive.android.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
@@ -25,8 +27,11 @@ class StartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val listener: (View) -> Unit = {
-
+            val intent = Intent(context, MainActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
         }
+        binding.btnStart.setOnClickListener(listener)
     }
 
 }
