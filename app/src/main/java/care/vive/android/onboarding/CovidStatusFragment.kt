cbc5f7bef0d99129f78b4cf1828481f61e185803
Carlos Alberto Murillo <carlosalbertomurillo@gmail.com>
2020-04-27 09:47:40 -0400

    Status view and some standariations to dimens

--- app/src/main/java/care/vive/android/onboarding/CovidStatusFragment.kt
+++ app/src/main/java/care/vive/android/onboarding/CovidStatusFragment.kt
@@ -1,34 +1,37 @@
 package care.vive.android.onboarding
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import care.vive.android.main.MainActivity
 import care.vive.android.util.BaseFragment
-import com.vive.android.databinding.FragmentStartBinding
+import com.vive.android.databinding.FragmentCovidStatusBinding
 
-class StartFragment : BaseFragment() {
+class CovidStatusFragment  : BaseFragment() {
 
     private val viewModelOnboarding: OnboardingViewModel by activityViewModels()
-    private lateinit var binding: FragmentStartBinding
+    private lateinit var binding: FragmentCovidStatusBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentStartBinding.inflate(inflater, container, false)
+        binding = FragmentCovidStatusBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val listener: (View) -> Unit = {
-            val directions = StartFragmentDirections.actionStartFragmentToCovidStatusFragment()
-            navigate(directions)
+            val intent = Intent(context, MainActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
         }
-        binding.btnStart.setOnClickListener(listener)
+        binding.btnNext.setOnClickListener(listener)
     }
 
 }
